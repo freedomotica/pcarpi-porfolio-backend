f0ed6edc9f2851a0109c75a80737b56f9d6af88d
Pablo Carpi <freedomotica@gmail.com>
2022-05-27 14:28:03 -0300

    Modificacon /login para que devuelva un objeto serializado como Json

--- src/main/java/com/miapp/springboot/Controller/AuthController.java
+++ src/main/java/com/miapp/springboot/Controller/AuthController.java
@@ -6,10 +6,13 @@ import com.miapp.springboot.JWT.JWTUtil;
 import com.miapp.springboot.model.Usuario;
 import com.miapp.springboot.service.IUsuarioService;
 import com.miapp.springboot.utils.Encriptado;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Optional;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.serializer.Serializer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,28 +30,40 @@ private Encriptado Encrip;
 private Usuario logUser;
 
 @Getter @Setter
-private class Respuesta {
+public class Respuesta implements Serializer {
     String token;
     String user;
-    public Respuesta(String token, String user) {
-        this.token = token;
-        this.user = user;
-    }
+    String message;
+
+        public Respuesta(String token, String user, String message) {
+            this.token = token;
+            this.user = user;
+            this.message = message;
+        }
+   
     
     public Respuesta() {
     }
+
+        @Override
+        public void serialize(Object object, OutputStream outputStream) throws IOException {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
     
 };
 
 
    // End point login
     @PostMapping ("/login")
-    public String Login(@RequestBody Usuario user){
+    public Respuesta Login(@RequestBody Usuario user){
         //busco usuario en la base de datos        
         Optional<Usuario> usLog = userServ.buscaUsuarioByUser(user.getUser());
+        //Objeto para armar el Json
+        Respuesta responseObj = new Respuesta();
         //Fail si no existe        
         if(!usLog.isPresent()){
-            return "FAIL - Usuario no encontrado";
+            responseObj.setMessage("FAIL - Usuario no encontrado");
+            return responseObj;
         }
         //si exite extraigo el Usuario
         usLog.ifPresent((Usuario userLog)->{
@@ -58,19 +73,21 @@ private class Respuesta {
         //comparar password
             //si verify es false retorna FAIL
         if(!Encrip.Verify(logUser.getPassword(), user.getPassword())){
-            return "FAIL - Contraseña no encontrada";
+            responseObj.setMessage("FAIL - Contraseña no encontrada");
+            return responseObj;
+            
         }
         //En este punto el usuario y contraseña estan ok
         //crear token
         String token = jwtutil.create(logUser.getId().toString(), logUser.getUser());
-        //Objeto para armar el Json
-        Respuesta responseObj = new Respuesta();
+        
         //Objeto para convertirn a Json - libreria de google
-        Gson gson = new Gson();
+        //Gson gson = new Gson();
         responseObj.setToken(token);
         responseObj.setUser(logUser.getUser());
         //Respondo convirtiendo a Json
-        return gson.toJson(responseObj);
+        //return gson.toJson(responseObj);
+        return responseObj;
         
         
     }
